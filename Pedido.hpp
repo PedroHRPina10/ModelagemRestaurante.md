9bcbbc9881de10f81cd96ad59ba3681c2118fce5
PedroHRPina10 <138832300+PedroHRPina10@users.noreply.github.com>
2023-07-12 15:10:42 -0300

    cpp and hpp files

--- Pedido.hpp
+++ Pedido.hpp
@@ -3,18 +3,19 @@
 
 #include <string>
 
-class Pedido {
+class Pedido
+{
 private:
     std::string cliente;
     std::string descricao;
     std::string status;
 
 public:
-    Pedido(const std::string& cliente, const std::string& descricao);
+    Pedido(const std::string &cliente, const std::string &descricao);
     std::string getCliente() const;
     std::string getDescricao() const;
     std::string getStatus() const;
-    void setStatus(const std::string& novoStatus);
+    void setStatus(const std::string &novoStatus);
 };
 
-#endif  // PEDIDO_HPP
+#endif
